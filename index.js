2de5d39e315fd14e76fb122f783b66b8dd19f917
sravittxof <travis.fox90@gmail.come>
2021-04-03 20:27:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    } 
+    return messages;
+}
+
+function countDown(positiveInteger) {
+    let i = positiveInteger + 1;
+    while( i > 0) {
+        console.log(i -1);
+        i--;        
+    }
+}
